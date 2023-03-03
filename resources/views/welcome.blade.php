189de3763ba781669033ed7e7214a9e54e6a91d1
Arsen Anay <arsenanai@gmail.com>
2023-03-03 16:24:31 +0600

    adding images locally

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{config('app.name', 'Refer to readme.md')}}</title>
-        <link rel="icon" href="https://steam.kz/wp-content/uploads/2022/10/cropped-STEAM_RGB-без_дескриптора_сайтқа-1536x523-1-32x32.png" sizes="32x32">
-        <link rel="icon" href="https://steam.kz/wp-content/uploads/2022/10/cropped-STEAM_RGB-без_дескриптора_сайтқа-1536x523-1-192x192.png" sizes="192x192">
-        <link rel="apple-touch-icon" href="https://steam.kz/wp-content/uploads/2022/10/cropped-STEAM_RGB-без_дескриптора_сайтқа-1536x523-1-180x180.png">
-        <meta name="msapplication-TileImage" content="https://steam.kz/wp-content/uploads/2022/10/cropped-STEAM_RGB-без_дескриптора_сайтқа-1536x523-1-270x270.png">
+        <link rel="icon" href="/cropped-STEAM_RGB-без_дескриптора_сайтқа-1536x523-1-32x32.png" sizes="32x32">
+        <link rel="icon" href="/cropped-STEAM_RGB-без_дескриптора_сайтқа-1536x523-1-192x192.png" sizes="192x192">
+        <link rel="apple-touch-icon" href="/cropped-STEAM_RGB-без_дескриптора_сайтқа-1536x523-1-180x180.png">
+        <meta name="msapplication-TileImage" content="/cropped-STEAM_RGB-без_дескриптора_сайтқа-1536x523-1-270x270.png">
     </head>
     <body class="text-my-indigo">
         <div id="entries-app"></div>
