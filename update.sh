a8d2b998e5568c6a25758e4a2e655dfbf8392510
Arsen Anay <arsenanai@gmail.com>
2023-04-29 12:12:34 +0600

    interactionless composer

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@ mysqldump -u root $DB_DATABASE > backups/$DB_DATABASE-$(date +"%d").sql
 php artisan down
 git fetch --all
 git reset --hard origin/main
-composer install
+composer install -n
 php artisan optimize:clear
 php artisan up
