b9b82021dc2e77906dcd9640f0787cbc66ac56a5
Arsen Anay <arsenanai@gmail.com>
2023-04-29 12:09:31 +0600

    passwordless backup

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 source .env
-mysqldump -u root -p $DB_DATABASE > backups/$DB_DATABASE-$(date +"%d").sql
+mysqldump -u root $DB_DATABASE > backups/$DB_DATABASE-$(date +"%d").sql
 php artisan down
 git fetch --all
 git reset --hard origin/main
