b06fb3e9009d2c0548fe4f3b90db37df64fcda1c
Arsen Anay <arsenanai@gmail.com>
2024-03-31 13:02:44 +0500

    update script changes

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@ source .env
 mysqldump -u root $DB_DATABASE > backups/$DB_DATABASE-$(date +"%d").sql
 php artisan down
 git fetch --all
-git reset --hard origin/check
+git reset --hard origin/robotics
 composer install -n
 npm i
 npm run build
